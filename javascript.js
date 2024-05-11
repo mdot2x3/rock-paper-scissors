647f17b3fc6209e02e257f2e8ef26518cc272bc4
mdot2x3 <mdot2x3@gmail.com>
2024-05-10 21:35:51 -0700

    feat: Add getHumanChoice function, edits
    built out new function.
    
    made edits to code and comments, changed
    const -> let to avoid potential errors.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 console.log("Hello, World!");
 
+// function to randomly select a RPS choice for the computer
 function getComputerChoice() {
-    // Getting a random integer between two values, inclusive
+    // getting a random integer between two values, inclusive
     // return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-    const result = Math.floor(Math.random() * (9) + 1);
-    //debug clg
+    let result = Math.floor(Math.random() * (9) + 1);
+    // debug clg
     console.log(result);
     // determine return value based on randomized result
     if (result >= 1 && result <=3) {
@@ -15,5 +16,26 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-//debug clg
-console.log(getComputerChoice())
+// debug clg
+console.log(getComputerChoice());
+
+// function to prompt user to make a RPS choice
+function getHumanChoice() {
+    // create while loop to run until valid input is received by user
+    let stillGoing = true;
+    while (stillGoing) {
+        let userInput = prompt("Please enter your choice (Rock, Paper, or Scissors)?");
+        // set user input to lowercase for conditional check
+        let userInputLower = userInput.toLowerCase();
+        // check if input is valid
+        if (userInputLower === "rock" || userInputLower === "paper" || userInputLower === "scissors") {
+            return userInputLower
+            stillGoing = false;
+        // if not valid, prompt user to try again
+        } else {
+            console.log("That isn't a valid choice. Please try again.");
+        }
+    }
+}
+// debug clg
+console.log(getHumanChoice());
